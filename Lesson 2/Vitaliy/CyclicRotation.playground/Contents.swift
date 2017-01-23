a436660f7a1ab930c760112e8bb9e6ea15d8b244
Vitaly Chernish <vitalychenrish@MacBook-Pro-Vitaly.local>
2017-01-24 01:22:04 +0700

    Lesson 2.1. done.

---
+++ Lesson 2/Vitaliy/CyclicRotation.playground/Contents.swift
@@ -0,0 +1,19 @@
+public func solution(_ A : inout [Int], _ K : Int) -> [Int] {
+    
+    guard K <= 100, K >= 0, !A.isEmpty, A.min()! >= -1000, A.max()! <= 1000, A.count - 1 <= 100 else {
+        print("Wrong entry parameters. Try another numbers.")
+        return []
+    }
+    
+    var lastElementForCurrentCycle = 0
+    
+    for _ in 0..<K {
+        lastElementForCurrentCycle = A.remove(at: A.count - 1)
+        A.insert(lastElementForCurrentCycle, at: A.startIndex)
+    }
+    
+    return A
+}
+
+var numbers = [-4]
+solution(&numbers, 0)
