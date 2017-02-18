8bd41a4b4fb51c08f878e9a949a655921e6a37fb
Vitaly Chernish <vitalychenrish@MacBook-Pro-Vitaly.local>
2017-02-18 23:00:57 +0200

    3.2. code clean

--- Lesson 3/Vitaliy/3_1_PermMissingElem.playground/Contents.swift
+++ Lesson 3/Vitaliy/3_1_PermMissingElem.playground/Contents.swift
@@ -1,12 +1,9 @@
 import UIKit
 
 public func solution(_ A : inout [Int]) -> Int {
-    guard !A.isEmpty else {
-        return 1
-    }
-    
+    guard !A.isEmpty else { return 1 }
     return Set<Int>(1...A.count + 2).subtracting(Set(A)).min()!
 }
 
-var numbers = [2, 3, 4, 5]
+var numbers = [1, 2, 3, 4, 5]
 solution(&numbers)
